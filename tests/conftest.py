94442b9f94aaf227f6c25853537d893f2b9f16a6
Alberto <albicento.ac@gmail.com>
2024-02-15 23:47:38 +0100

    test: using fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,42 @@ import pytest
 
 boa.env.enable_fast_mode()
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def accounts():
     return [boa.env.generate_address() for _ in range(10)]
 
+@pytest.fixture(scope="session")
+def alice(accounts):
+    return accounts[0]
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
+def bob(accounts):
+    return accounts[1]
+
+@pytest.fixture(scope="session")
 def admin():
     return boa.env.generate_address()
+
+@pytest.fixture(scope="session")
+def debt(admin):
+    with boa.env.prank(admin):
+        return boa.load("./tests/contracts/MockERC20.vy", "debt", "MOCK", 0, "Whatever", "Whatever")
+
+@pytest.fixture(scope="session")
+def collat(admin):
+    with boa.env.prank(admin):
+        return boa.load("./tests/contracts/MockERC20.vy", "collat", "MOCK", 0, "Whatever", "Whatever")
+
+@pytest.fixture(scope="session")
+def silo_asset(admin):
+    with boa.env.prank(admin):
+        return boa.load("./tests/contracts/MockERC20.vy", "MockERC20", "MOCK", 10, "Whatever", "Whatever")
+
+@pytest.fixture(scope="session")
+def silo(admin, silo_asset, debt, collat):
+    with boa.env.prank(admin):
+        silo = boa.load("contracts/Silo.vy", silo_asset.address)
+        collat.set_minter(silo.address, True)
+        debt.set_minter(silo.address, True)
+        silo.init_asset(collat.address, debt.address, silo_asset.address, False);
+        return silo
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,19 +1,9 @@
 import boa
 
-def test_deposit(admin, accounts) -> None:
+def test_deposit(admin, alice, silo, silo_asset) -> None:
     with boa.env.prank(admin):
-        mockERC20 = boa.load("./tests/contracts/MockERC20.vy", "MockERC20", "MOCK", 10, "Whatever", "Whatever")
-        collat = boa.load("./tests/contracts/MockERC20.vy", "collat", "MOCK", 1, "Whatever", "Whatever")
-        debt = boa.load("./tests/contracts/MockERC20.vy", "debt", "MOCK", 0, "Whatever", "Whatever")
-        silo = boa.load("contracts/Silo.vy", mockERC20.address)
-        mockERC20.mint(accounts[0], 1000);
-        collat.set_minter(silo.address, True)
-        debt.set_minter(silo.address, True)
-        collat.mint(accounts[0], 1000)
-        silo.init_asset(collat.address, debt.address, mockERC20.address, False);
+        silo_asset.mint(alice, 1000)
 
-
-    with boa.env.prank(accounts[0]):
-        mockERC20.approve(silo.address, 1000)
-        silo.deposit(mockERC20.address, accounts[0], 1000)
-
+    with boa.env.prank(alice):
+        silo_asset.approve(silo.address, 1000)
+        silo.deposit(silo_asset, alice, 1000)
