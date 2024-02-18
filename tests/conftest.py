4c68418a5bf40d24de66f07d51e5199188c51975
Alberto <albicento.ac@gmail.com>
2024-02-18 19:37:52 +0100

    test: added more aliases for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,42 +3,63 @@ import pytest
 
 boa.env.enable_fast_mode()
 
-@pytest.fixture(scope="session")
-def accounts():
-    return [boa.env.generate_address() for _ in range(10)]
 
 @pytest.fixture(scope="session")
-def alice(accounts):
-    return accounts[0]
+def alice():
+    return boa.env.generate_address("alice")
+
 
 @pytest.fixture(scope="session")
-def bob(accounts):
-    return accounts[1]
+def bob():
+    return boa.env.generate_address("bob")
+
 
 @pytest.fixture(scope="session")
 def admin():
-    return boa.env.generate_address()
+    return boa.env.generate_address("admin")
+
 
 @pytest.fixture(scope="session")
-def debt(admin):
+def debt(admin, silo, silo_asset):
     with boa.env.prank(admin):
-        return boa.load("./tests/contracts/MockERC20.vy", "debt", "MOCK", 0, "Whatever", "Whatever")
+        debt = boa.load("contracts/ShareDebtToken.vy", "debt", "debt", silo.address, silo_asset.address)
+        boa.env.alias(debt.address, "debt")
+        return debt
+
 
 @pytest.fixture(scope="session")
-def collat(admin):
+def collat(admin, silo, silo_asset):
     with boa.env.prank(admin):
-        return boa.load("./tests/contracts/MockERC20.vy", "collat", "MOCK", 0, "Whatever", "Whatever")
+        collat = boa.load("contracts/ShareCollateralToken.vy", "collateral", "coll", silo.address, silo_asset.address)
+        boa.env.alias(collat.address, "collat")
+        return collat
+
+
 
 @pytest.fixture(scope="session")
 def silo_asset(admin):
     with boa.env.prank(admin):
-        return boa.load("./tests/contracts/MockERC20.vy", "MockERC20", "MOCK", 10, "Whatever", "Whatever")
+        silo_asset = boa.load("./tests/contracts/MockERC20.vy", "MockERC20", "MOCK", 10, "Whatever", "Whatever")
+        boa.env.alias(silo_asset.address, "silo_asset")
+        return silo_asset
+
+
 
 @pytest.fixture(scope="session")
-def silo(admin, silo_asset, debt, collat):
+def silo(admin, silo_asset):
     with boa.env.prank(admin):
         silo = boa.load("contracts/Silo.vy", silo_asset.address)
-        collat.set_minter(silo.address, True)
-        debt.set_minter(silo.address, True)
-        silo.init_asset(collat.address, debt.address, silo_asset.address, False);
+        boa.env.alias(silo.address, "collat")
         return silo
+
+
+@pytest.fixture(autouse=True)
+def setup(admin, silo, silo_asset, collat, debt):
+    with boa.env.prank(admin):
+        silo.init_asset(collat.address, debt.address, silo_asset.address, False);
+    with boa.env.prank(silo.address):
+        collat.set_minter(admin, True)
+        debt.set_minter(admin, True)
+    yield
+
+boa.env
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -18,7 +18,7 @@ def test_withdraw(admin, alice, silo, silo_asset, collat):
         collat.approve(silo.address, 1000)
         silo.withdraw(silo_asset, alice, 1000)
 
-def test_borrow(admin, alice, bob, silo, silo_asset, debt):
+def test_borrow(admin, alice, bob, silo, silo_asset):
     with boa.env.prank(admin):
         silo_asset.mint(alice, 1000)
 
@@ -28,3 +28,19 @@ def test_borrow(admin, alice, bob, silo, silo_asset, debt):
 
     with boa.env.prank(bob):
         silo.borrow(silo_asset, bob, 100)
+
+def test_repay(admin, alice, bob, silo, silo_asset):
+    boa.env.alias("0xC6Acb7D16D51f72eAA659668F30A40d87E2E0551", "test-alias")
+    boa.env.alias("0x0000000000000000000000000000000000000000", "0x0") 
+    boa.env.alias("0x78548820b365886d05009F1127bf553603E5A836", "collat")
+    with boa.env.prank(admin):
+        silo_asset.mint(alice, 1000)
+
+    with boa.env.prank(alice):
+        silo_asset.approve(silo.address, 1000)
+        silo.deposit(silo_asset, alice, 1000)
+
+    with boa.env.prank(bob):
+        silo.borrow(silo_asset, bob, 100)
+        silo_asset.approve(silo.address, 100)
+        silo.repay(silo_asset, bob, 100)
